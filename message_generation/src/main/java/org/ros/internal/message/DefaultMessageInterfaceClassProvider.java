a362d90131f0bbb03c127d7a25e1b9ac7ae5c7d3
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-26 10:47:39 +0300

    Use java concurrent map
    Use java concurrent map, and idiom/api instead of guava , added final modifiers.

--- message_generation/src/main/java/org/ros/internal/message/DefaultMessageInterfaceClassProvider.java
+++ message_generation/src/main/java/org/ros/internal/message/DefaultMessageInterfaceClassProvider.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,39 +17,36 @@
 package org.ros.internal.message;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Maps;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author damonkohler@google.com (Damon Kohler)
  */
-public class DefaultMessageInterfaceClassProvider implements MessageInterfaceClassProvider {
+public final class DefaultMessageInterfaceClassProvider implements MessageInterfaceClassProvider {
 
-  private final Map<String, Class<?>> cache;
+    private final Map<String, Class<?>> cache = new ConcurrentHashMap<>();
 
-  public DefaultMessageInterfaceClassProvider() {
-    cache = Maps.newConcurrentMap();
-  }
 
-  @SuppressWarnings("unchecked")
-  @Override
-  public <T> Class<T> get(String messageType) {
-    if (cache.containsKey(messageType)) {
-      return (Class<T>) cache.get(messageType);
+    @SuppressWarnings("unchecked")
+    @Override
+    public final <T> Class<T> get(final String messageType) {
+        return (Class<T>) this.cache.computeIfAbsent(messageType, this::create);
     }
-    try {
-      String className = messageType.replace("/", ".");
-      Class<T> messageInterfaceClass = (Class<T>) getClass().getClassLoader().loadClass(className);
-      cache.put(messageType, messageInterfaceClass);
-      return messageInterfaceClass;
-    } catch (ClassNotFoundException e) {
-      return (Class<T>) RawMessage.class;
-    }
-  }
 
-  @VisibleForTesting
-  <T> void add(String messageType, Class<T> messageInterfaceClass) {
-    cache.put(messageType, messageInterfaceClass);
-  }
+    private final <T> Class<T> create(final String messageType) {
+        try {
+            final String className = messageType.replace("/", ".");
+            final Class<T> messageInterfaceClass = (Class<T>) this.getClass().getClassLoader().loadClass(className);
+            return messageInterfaceClass;
+        } catch (ClassNotFoundException e) {
+            return (Class<T>) RawMessage.class;
+        }
+    }
+
+    @VisibleForTesting
+    final <T> void add(final String messageType,final Class<T> messageInterfaceClass) {
+        this.cache.put(messageType, messageInterfaceClass);
+    }
 }
