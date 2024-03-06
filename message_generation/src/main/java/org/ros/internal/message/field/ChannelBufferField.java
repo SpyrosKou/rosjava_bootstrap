add852294e072491eb414d7b91f76ff95809bee1
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2024-03-06 17:33:32 +0200

    Added logging and extracted constant

--- message_generation/src/main/java/org/ros/internal/message/field/ChannelBufferField.java
+++ message_generation/src/main/java/org/ros/internal/message/field/ChannelBufferField.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2012 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,105 +20,114 @@ import com.google.common.base.Preconditions;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.ros.internal.message.MessageBuffers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.invoke.MethodHandles;
 import java.nio.ByteOrder;
 
 /**
  * @author damonkohler@google.com (Damon Kohler)
  */
 final class ChannelBufferField extends Field {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final String ORG_JBOSS_NETTY_BUFFER_CHANNEL_BUFFER = "org.jboss.netty.buffer.ChannelBuffer";
+    private final int size;
 
-  private final int size;
+    private ChannelBuffer value;
 
-  private ChannelBuffer value;
-
-  public static ChannelBufferField newVariable(FieldType type, String name, int size) {
-    return new ChannelBufferField(type, name, size);
-  }
-
-  private ChannelBufferField(FieldType type, String name, int size) {
-    super(type, name, false);
-    this.size = size;
-    value = MessageBuffers.dynamicBuffer();
-  }
-
-  @SuppressWarnings("unchecked")
-  @Override
-  public ChannelBuffer getValue() {
-    // Return a defensive duplicate. Unlike with copy(), duplicated
-    // ChannelBuffers share the same backing array, so this is relatively cheap.
-    return value.duplicate();
-  }
-
-  @Override
-  public void setValue(Object value) {
-    ChannelBuffer channelBufferValue = null;
-    if (value instanceof byte[]) {
-      channelBufferValue = ChannelBuffers.wrappedBuffer(ByteOrder.LITTLE_ENDIAN, byte[].class.cast(value));
-    } else if (value instanceof ChannelBuffer) {
-      channelBufferValue = ChannelBuffer.class.cast(value);
+    public static final ChannelBufferField newVariable(final FieldType type,final String name,final int size) {
+        return new ChannelBufferField(type, name, size);
     }
-    Preconditions.checkArgument(channelBufferValue.order() == ByteOrder.LITTLE_ENDIAN);
-    Preconditions.checkArgument(size < 0 || channelBufferValue.readableBytes() == size);
-    this.value = channelBufferValue;
-  }
 
-  @Override
-  public void serialize(ChannelBuffer buffer) {
-    if (size < 0) {
-      buffer.writeInt(value.readableBytes());
+    private ChannelBufferField(final FieldType type, final String name, final int size) {
+        super(type, name, false);
+        this.size = size;
+        this.value = MessageBuffers.dynamicBuffer();
     }
-    // By specifying the start index and length we avoid modifying value's
-    // indices and marks.
-    buffer.writeBytes(value, 0, value.readableBytes());
-  }
 
-  @Override
-  public void deserialize(ChannelBuffer buffer) {
-    int currentSize = size;
-    if (currentSize < 0) {
-      currentSize = buffer.readInt();
+    @SuppressWarnings("unchecked")
+    @Override
+    public final ChannelBuffer getValue() {
+        // Return a defensive duplicate. Unlike with copy(), duplicated
+        // ChannelBuffers share the same backing array, so this is relatively cheap.
+        return value.duplicate();
     }
-    value = buffer.readSlice(currentSize);
-  }
 
-  @Override
-  public String getMd5String() {
-    return String.format("%s %s\n", type, name);
-  }
+    @Override
+    public final void setValue(final Object value) {
+        final ChannelBuffer channelBufferValue;
+        if (value instanceof byte[]) {
+            channelBufferValue = ChannelBuffers.wrappedBuffer(ByteOrder.LITTLE_ENDIAN, byte[].class.cast(value));
+        } else if (value instanceof ChannelBuffer) {
+            channelBufferValue = ChannelBuffer.class.cast(value);
+        } else {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Null value for value:" + value + " type:" + this.getJavaTypeName());
+            }
+            channelBufferValue = null;
+        }
+        Preconditions.checkArgument(channelBufferValue.order() == ByteOrder.LITTLE_ENDIAN);
+        Preconditions.checkArgument(size < 0 || channelBufferValue.readableBytes() == size);
+        this.value = channelBufferValue;
+    }
 
-  @Override
-  public String getJavaTypeName() {
-    return "org.jboss.netty.buffer.ChannelBuffer";
-  }
+    @Override
+    public final void serialize(ChannelBuffer buffer) {
+        if (size < 0) {
+            buffer.writeInt(value.readableBytes());
+        }
+        // By specifying the start index and length we avoid modifying value's
+        // indices and marks.
+        buffer.writeBytes(value, 0, value.readableBytes());
+    }
 
-  @Override
-  public String toString() {
-    return "ChannelBufferField<" + type + ", " + name + ">";
-  }
+    @Override
+    public final void deserialize(ChannelBuffer buffer) {
+        int currentSize = size;
+        if (currentSize < 0) {
+            currentSize = buffer.readInt();
+        }
+        value = buffer.readSlice(currentSize);
+    }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = super.hashCode();
-    result = prime * result + ((value == null) ? 0 : value.hashCode());
-    return result;
-  }
+    @Override
+    public final String getMd5String() {
+        return String.format("%s %s\n", type, name);
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (!super.equals(obj))
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    ChannelBufferField other = (ChannelBufferField) obj;
-    if (value == null) {
-      if (other.value != null)
-        return false;
-    } else if (!value.equals(other.value))
-      return false;
-    return true;
-  }
+    @Override
+    public final String getJavaTypeName() {
+        return ORG_JBOSS_NETTY_BUFFER_CHANNEL_BUFFER;
+    }
+
+    @Override
+    public final String toString() {
+        return "ChannelBufferField<" + type + ", " + name + ">";
+    }
+
+    @Override
+    public final int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final ChannelBufferField other = (ChannelBufferField) obj;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
 }
