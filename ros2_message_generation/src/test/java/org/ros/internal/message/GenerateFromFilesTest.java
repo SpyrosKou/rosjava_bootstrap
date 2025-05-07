6eb2119f16b5bfdf69fba7529987d1b390d4db52
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2025-05-07 22:05:36 +0300

    test: use the new interfaces for testing #8

--- ros2_message_generation/src/test/java/org/ros/internal/message/GenerateFromFilesTest.java
+++ ros2_message_generation/src/test/java/org/ros/internal/message/GenerateFromFilesTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 Spyros Koukas.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.ros.internal.message;
 
 import org.junit.jupiter.api.AfterEach;
@@ -17,7 +32,7 @@ import java.util.List;
  */
 public class GenerateFromFilesTest {
     private static final String targetDir = "build/generated-sources/java";
-    private static final String sourcesDir = "src/test/resources/";
+    private static final String sourcesDir = "src/test/resources/common_interfaces";
     private static File targetDirFile;
 
     private final List<File> packageDirectories = new ArrayList<>();
